5d17dcd3a591742d5d64fd1d35f02d57888e5bd4
Emzilla-01 <10577954+Emzilla-01@users.noreply.github.com>
2023-02-27 23:24:58 -0500

    improve the sql, results preserved

--- fb_data_sql.sql
+++ fb_data_sql.sql
@@ -1,3 +1,4 @@
+-- try at https://www.db-fiddle.com/f/4Fy9rAgzr3f6ex1n3W2WZ5/7
 --------------------------------------------------------------------------------
 -- Schema SQL
 --------------------------------------------------------------------------------
@@ -15,13 +16,21 @@ INSERT INTO unlabeled_image_predictions (image_id, score) VALUES
 ('115','0.5309'), ('417','0.7168'), ('706','0.9649'), ('166','0.2507'), ('991','0.4191'), ('465','0.0895'), ('53','0.8169'),
 ('971','0.9871');
 
-alter table unlabeled_image_predictions add column weak_label int default null;
-update unlabeled_image_predictions set weak_label = ROUND(CAST(score AS numeric), 0); 
--- Emy tried other assignments in the DDL dialogue at https://www.db-fiddle.com/f/4Fy9rAgzr3f6ex1n3W2WZ5/4 but only got those to work in DQL
-alter table unlabeled_image_predictions add column row_number_asc int default null;
-alter table unlabeled_image_predictions add column row_number_desc int default null;
-alter table unlabeled_image_predictions add column subsample_desc int default null;
-alter table unlabeled_image_predictions add column subsample_asc int default null;
+
+alter table unlabeled_image_predictions 
+    add column weak_label int default null,
+    add column row_number_asc int default null,
+    add column row_number_desc int default null,
+    add column subsample_desc int default null,
+    add column subsample_asc int default null
+;
+
+update unlabeled_image_predictions set weak_label = ROUND(CAST(score AS numeric), 0);
+
+CREATE TABLE IF NOT EXISTS dim_table_attrs (
+table_name varchar,
+table_count int
+);
 
 --------------------------------------------------------------------------------
 /*Our sampling strategy is to order the images in decreasing order of scores and sample every
@@ -32,47 +41,56 @@ samples.*/
 -- Query SQL
 --------------------------------------------------------------------------------
 
--- assign row_number_desc
-WITH view_sample AS
-(SELECT ROW_NUMBER () OVER (order by score desc) as row_number, image_id
-    FROM unlabeled_image_predictions order by score desc) 
-UPDATE unlabeled_image_predictions set row_number_desc = view_sample.row_number
-FROM view_sample
-WHERE unlabeled_image_predictions.image_id = view_sample.image_id;
+-- count rows for asc/desc assignments in one-pass
+insert into dim_table_attrs (table_count, table_name)
+(select 
+  count(*) as table_count,
+  'unlabeled_image_predictions' as table_name
+  from unlabeled_image_predictions
+)
+;
 
--- assign row_number_asc
-WITH view_sample AS
-(SELECT ROW_NUMBER () OVER (order by score asc) as row_number, image_id
-    FROM unlabeled_image_predictions order by score asc) 
-UPDATE unlabeled_image_predictions set row_number_asc = view_sample.row_number
-FROM view_sample
-WHERE unlabeled_image_predictions.image_id = view_sample.image_id;
 
---assign subsample_asc
-WITH view_sample AS
-(SELECT MOD(row_number_asc, 3)-1 as subsample_asc, image_id
-    FROM unlabeled_image_predictions order by row_number_asc asc)    
-UPDATE unlabeled_image_predictions set subsample_asc = view_sample.subsample_asc
-FROM view_sample
-WHERE unlabeled_image_predictions.image_id = view_sample.image_id;
+with tablecount as 
+ (select max(table_count) table_count
+from dim_table_attrs 
+where table_name= 'unlabeled_image_predictions' 
+group by table_name),
 
---assign subsample_desc
-WITH view_sample AS
-(SELECT MOD(row_number_desc, 3)-1 as subsample_desc, image_id
-    FROM unlabeled_image_predictions order by row_number_desc asc)    
-UPDATE unlabeled_image_predictions set subsample_desc = view_sample.subsample_desc
-FROM view_sample
-WHERE unlabeled_image_predictions.image_id = view_sample.image_id;
+view_sample_desc as 
+    (SELECT 
+        ROW_NUMBER() OVER (order by score desc) as row_number_desc,     
+        1 + tablecount.table_count - ROW_NUMBER() OVER (order by score desc)  as row_number_asc,
+        MOD( 1 + tablecount.table_count - ROW_NUMBER() OVER (order by score desc), 3)-1 as subsample_asc,
+        MOD(ROW_NUMBER() OVER (order by score desc), 3)-1 as subsample_desc,
+     	image_id
+
+    FROM unlabeled_image_predictions
+    left join tablecount on true
+    )
+
+
+UPDATE unlabeled_image_predictions 
+set
+subsample_desc = view_sample_desc.subsample_desc,
+subsample_asc = view_sample_desc.subsample_asc,
+row_number_asc = view_sample_desc.row_number_asc,
+row_number_desc = view_sample_desc.row_number_desc
+from view_sample_desc
+WHERE unlabeled_image_predictions.image_id = view_sample_desc.image_id;
+
 
 create temp view vw_hi_5 as 
     select * from unlabeled_image_predictions 
-    where (weak_label = 1 and subsample_desc = 0) order by score desc limit 5 ;
+    where (weak_label = 1 and subsample_desc = 0) 
+    order by score desc limit 5;
 create temp view vw_lo_5 as 
     select * from unlabeled_image_predictions
-    where (weak_label = 0 and subsample_asc = 0) order by score asc limit 5 ;
+    where (weak_label = 0 and subsample_asc = 0) 
+    order by score asc limit 5;
 
 
-select image_id, weak_label from vw_hi_5 union
+-- final output per prompt
+select image_id, weak_label from vw_hi_5
+union all
 select image_id, weak_label from vw_lo_5 order by image_id asc;
-
---------------------------------------------------------------------------------
